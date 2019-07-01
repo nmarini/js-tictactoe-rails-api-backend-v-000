cd1a5d1d4227dcf14f678c731a292b1b93d602df
Nicolas Marini <nmarini44@gmail.com>
2019-07-01 20:01:30 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,27 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+
+  def index
+
+  end
+
+  def create
+    @game = Game.create(game_params)
+    render json: @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+
+  end
+
+  private
+
+  def game_params
+    params.permit(state: [])
+  end
 
 end
